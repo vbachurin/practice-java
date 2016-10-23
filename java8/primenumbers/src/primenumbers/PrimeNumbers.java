66725ccd2d1b4d38766dc887b7b3cf5097f96ca5
Vlad Bachurin <vbachurin@talend.com>
2016-10-24 01:25:43 +0300

    Exercising with Java 8 Streaming API

---
+++ java8/primenumbers/src/primenumbers/PrimeNumbers.java
@@ -0,0 +1,19 @@
+package primenumbers;
+
+import java.util.stream.IntStream;
+
+public class PrimeNumbers {
+    
+    public long countPrimes(int upTo) {
+        return IntStream.range(1, upTo).filter(this::isPrime).count();
+    }
+
+    private boolean isPrime(int max) {
+        return IntStream.range(2, max).allMatch(x -> (max % x) != 0);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new PrimeNumbers().countPrimes(3));
+    }
+
+}
